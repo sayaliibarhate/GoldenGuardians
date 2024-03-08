a1d3c78509d44cb855fccbd66456721ebf937e88
Sayali Barhate <sabarhate.27@gmail.com>
2024-03-08 18:21:58 +0530

    Change in login page

--- Pages/Login.cshtml.cs
+++ Pages/Login.cshtml.cs
@@ -4,6 +4,9 @@ using System.Security.Cryptography.X509Certificates;
 using MySql.Data.MySqlClient;
 using Mysqlx.Crud;
 using GoldenGuardians.Models;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Reflection;
+using System.Xml.Linq;
 
 namespace GoldenGuardians.Pages
 {
@@ -15,6 +18,18 @@ namespace GoldenGuardians.Pages
         {
               string email = Request.Form["email"];
               string password = Request.Form["pass"];
-        }
+
+			using (MySqlConnection con = new MySqlConnection("server=localhost;username=root;database=gyg;port=3306;password=sau@271202"))
+			{
+				//opening connection
+				con.Open();
+
+				string query = "select * from log where email = ?email;";
+
+				MySqlCommand cmd = new MySqlCommand(query, con);
+
+				con.Close();
+			}
+		}
     }
 }
